9db83e1173b06cba4d2fcf082290ec5812f9cd61
Prasanth <prasanthcadirvele@gmail.com>
2023-12-20 18:21:01 +0000

    some minor modif in UserC then created a new entity Enseignant after after fixing some errors

--- groupesTesteurs/src/Controller/UserController.php
+++ groupesTesteurs/src/Controller/UserController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class UserController extends AbstractController
 {
@@ -17,19 +19,41 @@ class UserController extends AbstractController
         $this->userRepository = $userRepository;
     }
 
-
-    public function getAllUsers(): Response
+    /**
+     * @Route("/users", methods={"GET"}, name="get_all_users")
+     */
+    public function getAllUsers(): JsonResponse
     {
-        return $this->json([
+        $users = $this->userRepository->findAll();
 
-        ]);
+        if (empty($users)) {
+            return $this->json(['error' => 'No users found'], 404);
+        }
+
+        $formattedUsers = [];
+        foreach ($users as $user) {
+            $formattedUsers[] = [
+                'id' => $user->getId(),
+                'email' => $user->getEmail(),
+                // Add more fields as needed
+            ];
+        }
+
+        return $this->json($formattedUsers);
     }
 
-
-    public function getUserById(Request $request): Response
+    /**
+     * @Route("/users/{id}", methods={"GET"}, name="get_user_by_id")
+     * @ParamConverter("user", class="App\Entity\User")
+     */
+    public function getUserById(User $user): JsonResponse
     {
-        return $this->json([
+        $formattedUser = [
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            // Add more fields as needed
+        ];
 
-        ]);
+        return $this->json($formattedUser);
     }
 }
---
+++ groupesTesteurs/src/Entity/Enseignant.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EnseignantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EnseignantRepository::class)]
+class Enseignant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
