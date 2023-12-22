5286721824e1d7f6b026ce93c692259e3f262c63
Prasanth <prasanthcadirvele@gmail.com>
2023-12-22 16:44:59 +0000

    modif in UserController

--- groupesTesteurs/src/Controller/UserController.php
+++ groupesTesteurs/src/Controller/UserController.php
@@ -24,6 +24,11 @@ class UserController extends AbstractController
      */
     public function getAllUsers(): JsonResponse
     {
+        // Check if the logged-in user is an admin
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException('You do not have permission to view all users.');
+        }
+
         $users = $this->userRepository->findAll();
 
         if (empty($users)) {
@@ -42,18 +47,120 @@ class UserController extends AbstractController
         return $this->json($formattedUsers);
     }
 
+     /**
+     * @Route("/enseignants", methods={"GET"}, name="get_all_enseignants")
+     */
+    public function getAllEnseignants(): JsonResponse
+    {
+        // Check if the logged-in user is an admin
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException('You do not have permission to view all enseignants.');
+        }
+
+        $enseignants = $this->userRepository->findBy(['type' => 'enseignant']);
+
+        $formattedEnseignants = [];
+        foreach ($enseignants as $enseignant) {
+            $formattedEnseignants[] = [
+                'id' => $enseignant->getId(),
+                'email' => $enseignant->getEmail(),
+            ];
+        }
+
+        return $this->json($formattedEnseignants);
+    }
+
+    /**
+     * @Route("/admins", methods={"GET"}, name="get_all_admins")
+     */
+    public function getAllAdmins(): JsonResponse
+    {
+        // Check if the logged-in user is an admin
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException('You do not have permission to view all admins.');
+        }
+
+        $admins = $this->userRepository->findBy(['type' => 'admin']);
+
+        $formattedAdmins = [];
+        foreach ($admins as $admin) {
+            $formattedAdmins[] = [
+                'id' => $admin->getId(),
+                'email' => $admin->getEmail(),
+        
+            ];
+        }
+
+        return $this->json($formattedAdmins);
+    }
+
     /**
      * @Route("/users/{id}", methods={"GET"}, name="get_user_by_id")
      * @ParamConverter("user", class="App\Entity\User")
      */
-    public function getUserById(User $user): JsonResponse
+    public function getUserById(int $id): JsonResponse
     {
+        // Check if the logged-in user is an admin
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException('You do not have permission to view user details.');
+        }
+
+        $user = $this->userRepository->find($id);
+
+        if (!$user) {
+            return $this->json(['error' => 'User not found'], 404);
+        }
+
         $formattedUser = [
             'id' => $user->getId(),
             'email' => $user->getEmail(),
-            // Add more fields as needed
+
         ];
 
         return $this->json($formattedUser);
     }
+
+    /**
+     * @Route("/users/{id}/edit", methods={"GET", "POST"}, name="edit_user")
+     */
+    public function editUser(Request $request, int $id): Response
+    {
+        $user = $this->userRepository->find($id);
+
+        if (!$user) {
+            return $this->json(['error' => 'User not found'], 404);
+        }
+
+        // Check if the logged-in user is the admin or the user being edited
+        $loggedInUser = $this->getUser();
+        if (!$loggedInUser || ($loggedInUser->getId() !== $user->getId() && !$this->isGranted('ROLE_ADMIN'))) {
+            throw new AccessDeniedException('You do not have permission to edit this user.');
+        }
+
+        // Logic to handle user editing
+
+        return $this->json(['message' => 'User edited successfully']);
+    }
+
+    /**
+     * @Route("/users/{id}/delete", methods={"DELETE"}, name="delete_user")
+     */
+    public function deleteUser(int $id): Response
+    {
+        $user = $this->userRepository->find($id);
+
+        if (!$user) {
+            return $this->json(['error' => 'User not found'], 404);
+        }
+
+        // Check if the logged-in user is an admin
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException('You do not have permission to delete users.');
+        }
+
+        // User deletion logic here
+
+        return $this->json(['message' => 'User deleted successfully']);
+    }
 }
+
